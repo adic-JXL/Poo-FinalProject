3155ca1096a013e85c74981dee80b04014381a28
ManuJei <polosalgadomanueldavid@gmail.com>
2026-05-07 22:23:40 -0500

    Puerta

--- Scripts/jugador.gd
+++ Scripts/jugador.gd
@@ -4,38 +4,52 @@ class_name Jugador
 signal estado_cambiado(nuevo_estado: StringName)
 signal vida_cambiada(vida_actual: int)
 
+@export_group("Configuración")
 @export var nombre: String = "Protagonista"
 @export var vida: int = 3
-@export var velocidad_base: float = 260.0
-@export var fuerza_salto: float = 420.0
-@export var aceleracion: float = 1600.0
-@export var desaceleracion: float = 1800.0
+
+@export_group("Movimiento")
+@export var velocidad_base = 260.0
+@export var fuerza_salto = 420.0
+@export var aceleracion = 1600.0
+@export var desaceleracion = 1800.0
 
 var estado_actual: StringName = &"normal"
 var _gravedad: float = 0.0
 var _direccion_actual: float = 1.0
+var _escala_original_x: float = 1.0 # Variable para evitar que se deforme
 
 @onready var visual: Node2D = $Visual
 
 
 func _ready() -> void:
 	_gravedad = float(ProjectSettings.get_setting("physics/2d/default_gravity"))
+	
+	# Guardamos la escala horizontal que pusiste en el editor
+	_escala_original_x = visual.scale.x
+	
 	emit_signal("vida_cambiada", vida)
 	emit_signal("estado_cambiado", estado_actual)
 
 
 func _physics_process(delta: float) -> void:
+	# Aplicar gravedad
 	if not is_on_floor():
 		velocity.y += _gravedad * delta
 
+	# Manejar movimiento horizontal
 	var direccion := obtener_input()
 	mover(direccion, delta)
 
+	# Manejar salto
 	if Input.is_action_just_pressed("saltar"):
 		saltar()
 
 	move_and_slide()
-	_actualizar_orientacion(direccion)
+	
+	# Solo actualizamos la orientación si hay movimiento
+	if not is_zero_approx(direccion):
+		_actualizar_orientacion(direccion)
 
 
 func obtener_input() -> float:
@@ -43,8 +57,8 @@ func obtener_input() -> float:
 
 
 func mover(direccion: float, delta: float) -> void:
-	var velocidad_objetivo := direccion * velocidad_base
-	var ajuste := aceleracion if not is_zero_approx(direccion) else desaceleracion
+	var velocidad_objetivo = direccion * velocidad_base
+	var ajuste = aceleracion if not is_zero_approx(direccion) else desaceleracion
 	velocity.x = move_toward(velocity.x, velocidad_objetivo, ajuste * delta)
 
 
@@ -67,8 +81,6 @@ func cambiar_estado(nuevo_estado: StringName) -> void:
 
 
 func _actualizar_orientacion(direccion: float) -> void:
-	if is_zero_approx(direccion):
-		return
-
 	_direccion_actual = signf(direccion)
-	visual.scale.x = _direccion_actual
+	# Multiplicamos la dirección (1 o -1) por la escala que guardamos en _ready
+	visual.scale.x = _direccion_actual * _escala_original_x
